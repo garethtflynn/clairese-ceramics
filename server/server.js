5b0e55eaefffce7aa9aebf103051d1aeb7804326
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-12-07 21:22:44 -0500

    added more sevrer  code

--- server/server.js
+++ server/server.js
@@ -3,5 +3,16 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-//need schemas when made
+const { typeDefs, resolvers } = require('./schemas');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 
