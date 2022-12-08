56caba035b9ca9f07a188a835d6fcac577eff445
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-12-07 20:58:59 -0500

    added intial server file structure as well as intitial model structure

---
+++ server/models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 15.00
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    default: 0
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
