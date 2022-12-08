e828bdd432323ab471c4f3f4db7bc9dc7b30960b
CBrunote <christian.bruno1995@gmail.com>
2022-12-08 18:56:02 -0500

    Added login and logut pages with styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // import { useMutation } from '@apollo/client';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import { LOGIN } from '../utils/mutations';
 // import Auth from '../utils/auth';
 
