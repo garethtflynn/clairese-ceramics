a3f1ef80479e76a489eb40c57da32bcfceeddffc
CBrunote <christian.bruno1995@gmail.com>
2022-12-08 21:16:38 -0500

    fixed issues with tailwind and added mutations, reducers, and actions

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { StoreProvider } from './utils/GlobalState';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Routes>
+              <Route 
+                path="/Login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/Signup" 
+                element={<Signup />} 
+              />
+            </Routes>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
