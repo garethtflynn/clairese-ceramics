f6932507a9be090a2e8ecdbfdf71395c29af1e81
Ashlynn Landry <ashclandry@gmail.com>
2022-12-12 16:07:03 -0500

    added banner jsx

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+function banner() {
+    return (
+        <div className="bg-[#DDBEA9]">
+            <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+                <p className="text-center font-medium text-white">
+                    New Collection Coming Soon!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default banner;
