89ff290cd1492da7d21ae026a5eaab7bf5a41e1d
Gareth Flynn <gareth.t.flynn@gmail.com>
2022-12-12 20:26:07 -0500

    committing components

---
+++ client/src/components/ProductList/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import ProductItem from "../ProductItem";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODUCTS } from "../../utils/";
+import { idbPromise } from "../../utils/";
+
+function ProductList() {
+  const [state, dispatch] = useStoreContext();
+  const { currentCategory } = state;
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+      data.products.forEach((product) => {
+        idbPromise("products", "put", product);
+      });
+    } else if (!loading) {
+      idbPromise("products", "get").then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
+      });
+    }
+  }, [data, loading, dispatch]);
+  
+  
+  return (
+      <div>
+        
+      </div>);
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ProductList from '../components/ProductList'
+
+
+
+const Home = () => { 
+  return (
+    <div className=''>
+
+        <ProductList />
+
+    </div>
+  )
+
+
+};
+
+export default Home;
