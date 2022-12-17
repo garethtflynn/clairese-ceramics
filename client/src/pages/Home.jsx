9f6107ba703273f4371783d114a2dc5a3d7e9533
Ashlynn Landry <ashclandry@gmail.com>
2022-12-17 03:21:23 -0500

    updated font on home screen

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,45 +4,50 @@ import curvymug1 from '../assets/curvymug1.jpg';
 import set1 from '../assets/set1.jpg';
 import {Link} from 'react-router-dom'
 
+{/* <div className="mx-auto mb-20 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"> */}
 
 function Home() {
     return (
-        <div id="carouselExampleSlidesOnly" class="carousel slide relative" data-bs-ride="carousel">
-            <div class="carousel-inner relative w-full overflow-hidden">
-                <div class="carousel-item active relative float-left w-full">
+        <div id="carouselExampleSlidesOnly" className="carousel slide relative" data-bs-ride="carousel">
+            <div className="carousel-inner relative w-full overflow-hidden">
+                <div className="carousel-item active relative float-left w-full">
+                    <h1 className="absolute top-10 text-right right-2 sm:right-20 text-white italic px-20 py-2 m-2 fontFamily text-2xl sm:text-5xl">handmade in georgia <br />by ashlynn claire<br /></h1>
                     <img
                     src={twinmug1}
                     class="block w-full"
                     alt="Twin Mug"
                     />
-                    <Link to='/products'><button class="rounded absolute bottom-10 left-10 bg-[#A5A58D] text-white italic px-20 py-2 shadow hover:shadow-lg m-2">Shop Collection</button></Link>
+                    <Link to='/products'><button className="rounded-md absolute bottom-10 sm:bottom-20 left-20 bg-[#A5A58D] text-white italic px-20 py-2 shadow hover:shadow-lg m-2">Shop Collection</button></Link>
 
                 </div>
-                <div class="carousel-item relative float-left w-full">
+                <div className="carousel-item relative float-left w-full">
+                    <h1 className="absolute top-10 text-right right-2 sm:right-20 text-white italic px-20 py-2 m-2 fontFamily text-2xl sm:text-5xl">handmade in georgia <br />by ashlynn claire<br /></h1>
                     <img
                     src={pitcher1}
                     class="block w-full"
                     alt="Pitcher"
                     />
-                    <Link to='/products'><button class="rounded absolute bottom-10 left-10 bg-[#A5A58D] text-white italic px-20 py-2 shadow hover:shadow-lg m-2">Shop Collection</button></Link>
+                    <Link to='/products'><button className="rounded-md absolute bottom-10 sm:bottom-20 left-20 bg-[#A5A58D] text-white italic px-20 py-2 shadow hover:shadow-lg m-2">Shop Collection</button></Link>
 
                 </div>
                 <div class="carousel-item relative float-left w-full">
+                    <h1 className="absolute top-10 text-right right-2 sm:right-20 text-white italic px-20 py-2 m-2 fontFamily text-xl sm:text-5xl">handmade in georgia <br />by ashlynn claire<br /></h1>
                     <img
                     src={curvymug1}
-                    class="block w-full"
+                    className="block w-full"
                     alt="Curvy Mug"
                     />
-                    <Link to='/products'><button class="rounded absolute bottom-10 left-10 bg-[#A5A58D] text-white italic px-20 py-2 shadow hover:shadow-lg m-2">Shop Collection</button></Link>
+                    <Link to='/products'><button className="rounded-md absolute bottom-10 sm:bottom-20 left-20 bg-[#A5A58D] text-white italic px-20 py-2 shadow hover:shadow-lg m-2">Shop Collection</button></Link>
                 </div>
 
                 <div class="carousel-item relative float-left w-full">
+                    <h1 className="absolute top-10 text-right right-2 sm:right-20 text-white italic px-20 py-2 m-2 fontFamily text-2xl sm:text-5xl">handmade in georgia <br />by ashlynn claire<br /></h1>
                     <img
                     src={set1}
-                    class="block w-full"
+                    className="block w-full"
                     alt="Full Set"
                     />
-                    <Link to='/products'><button class="rounded absolute bottom-10 left-10 bg-[#A5A58D] text-white italic px-20 py-2 shadow hover:shadow-lg m-2">Shop Collection</button></Link>
+                    <Link to='/products'><button className="rounded-md absolute bottom-10 sm:bottom-20 left-20 bg-[#A5A58D] text-white italic px-20 py-2 shadow hover:shadow-lg m-2">Shop Collection</button></Link>
                 </div>
             </div>
         </div>
