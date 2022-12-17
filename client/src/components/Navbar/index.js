3bd73a3493b3ae965adb33d6297306a1130fe326
Ashlynn Landry <ashclandry@gmail.com>
2022-12-17 01:46:04 -0500

    fixed issues with Navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,185 +1,161 @@
-import React, { useState } from "react";
+import { Fragment } from 'react'
 import logo from '../../assets/cclogo.png';
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, ShoppingCartIcon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-import { FaBars, FaTimes, FaUser, FaShoppingCart } from "react-icons/fa";
-//import { Link } from "react-router-dom";
+const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'Shop', href: './Products', current: true },
+  { name: 'About', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
+]
 
-const NavBar = () => {
-  const [menu, setMenu] = useState(false);
-  const menuLinks = [
-    {
-      id: 1,
-      item: "Shop Collections",
-      itemLink: "a",
-    },
-    {
-      id: 2,
-      item: "About",
-      itemLink: "b",
-    },
-    {
-      id: 3,
-      item: "Contact",
-      itemLink: "c",
-    },
-  ];
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
-  const [profile, setProfile] = useState(false);
-  const profileLinks = [
-    {
-      id: 1,
-      item: "Settings",
-      itemLink: "d",
-    },
-    {
-      id: 2,
-      item: "Orders",
-      itemLink: "e",
-    },
-    {
-      id: 3,
-      item: "Sign out",
-      itemLink: "f",
-    },
-  ];
-
+export default function Example() {
   return (
-    <div className="sticky mx-auto max-w-7xl px-2 py-6 sm:px-6 sm:py-6 lg:px-8 ">
-      <div className="relative flex h-16 items-center justify-between">
-        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          {/* Mobile menu */}
-          <div
-            onClick={() => setMenu(!menu)}
-            className="rounded inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-[#B0BEC7] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-            aria-controls="mobile-menu"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-
-            {menu ? <FaTimes size={20} /> : <FaBars size={20} />}
-
-            <div>
-              {/* hide menu until hamburger is clicked */}
-              <div
-                className=""
-                role="menu"
-                aria-orientation="vertical"
-                labelled="user-menu-item"
-              >
-                {menu && (
-                  <ul className="absolute left-0 z-18 mt-12 w-48 origin-top-left rounded-md bg-white py-1">
-                    {menuLinks.map(({ id, item, itemLink }) => (
-                      <li key={id} className="px-4 py-2 text-gray-800 italic">
-                        {item}
-                        {itemLink}
-                      </li>
+    <Disclosure as="nav" className="bg-white">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 py-6 sm:px-6 sm:py-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="rounded inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-[#B0BEC7] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <a href='/'>
+                    <img
+                      className="block h-28 w-auto lg:hidden"
+                      src={logo}
+                      alt="Clairese Ceramics Logo"
+                    />
+                  </a>
+                  
+                  <a href='./'>
+                    <img
+                      className="hidden h-28 w-auto lg:block"
+                      src={logo}
+                      alt="Clairese Ceramics Logo"
+                    />
+                  </a>
+                  
+                </div>
+                <div className="hidden py-12 sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-[#B0BEC7] text-white' : 'text-black hover:bg-[#B0BEC7] hover:text-white',
+                          'px-3 py-2 rounded-md text-md font-light italic'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
                     ))}
-                  </ul>
-                )}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <a href='./Cart'>
+                  <button
+                    type="button"
+                    className="rounded-full p-2 text-black hover:bg-[#B0BEC7] hover:text-white hover:p-2"
+                  >
+                    <span className="sr-only">View Items</span>
+                    <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </a>
+                
+
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="rounded-full p-2 text-black hover:bg-[#B0BEC7] hover:text-white hover:p-2">
+                      <span className="sr-only">Open user menu</span>
+                      <UserIcon className="h-6 w-6" aria-hidden="true" />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'rounded mx-1 hover:bg-[#B0BEC7] hover:text-white' : '', 'block px-4 py-2 text-sm text-black')}
+                          >
+                            Sign In
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'rounded mx-1 hover:bg-[#B0BEC7] hover:text-white' : '', 'block px-4 py-2 text-sm text-black')}
+                          >
+                            Billing
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'rounded mx-1 hover:bg-[#B0BEC7] hover:text-white' : '', 'block px-4 py-2 text-sm text-black')}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div className="flex flex-shrink-0 items-center">
-            <img
-              className="block h-28 w-auto lg:hidden"
-              src={logo}
-              alt="Your Company"
-            />
-            <img
-              className="hidden h-28 w-auto lg:block"
-              src={logo}
-              alt="Your Company"
-            />
-          </div>
-
-          <div className="hidden sm:ml-6 sm:block">
-            <div className="flex space-x-4 py-12">
-              <a
-                href="./Login"
-                className="text-black font-light italic hover:bg-[#B0BEC7] hover:text-white px-3 py-2 rounded-md text-md font-regular"
-                aria-current="page"
-              >
-                Shop Collections
-              </a>
-
-              <a
-                href="./Login"
-                className="text-black font-light italic hover:bg-[#B0BEC7] hover:text-white px-3 py-2 rounded-md text-md font-regular"
-              >
-                About
-              </a>
-
-              <a
-                href="./Login"
-                className="text-black font-light italic hover:bg-[#B0BEC7] hover:text-white px-3 py-2 rounded-md text-md font-regular"
-              >
-                Contact
-              </a>
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-[#B0BEC7] text-white' : 'text-black hover:bg-[#B0BEC7] hover:text-white',
+                    'block px-3 py-2 rounded-md text-md font-light italic'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
             </div>
-          </div>
-        </div>
-        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <button
-            type="button"
-            className="rounded-full p-2 text-black hover:bg-[#B0BEC7] hover:text-white hover:p-2"
-          >
-            <span className="sr-only">View notifications</span>
-
-            <FaShoppingCart size={20} />
-            <div>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="./src/pages/Cart"
-                alt="cart"
-              >
-                {" "}
-              </a>
-            </div>
-          </button>
-
-          {/* Profile dropdown */}
-          <div className="relative ml-3">
-            <div>
-              {/* change state to toggle user menu */}
-              <div
-                onClick={() => setProfile(!profile)}
-                className="rounded-full p-2 text-black hover:bg-[#B0BEC7] hover:text-white hover:p-2"
-              >
-                <span className="sr-only">profile menu</span>
-
-                <FaUser size={20} />
-              </div>
-            </div>
-
-            <div>
-              {/* hide menu until profile is clicked */}
-              <div
-                className=""
-                role="menu"
-                aria-orientation="vertical"
-                labelled="user-menu-item"
-              >
-                {profile && (
-                  <ul className="absolute right-0 z-18 mt-8 w-48 origin-top-right rounded-md bg-white py-1">
-                    {profileLinks.map(({ id, item, itemLink }) => (
-                      <li key={id} className="px-4 py-2 text-gray-800 italic">
-                        {item}
-                        {itemLink}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default NavBar;
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
