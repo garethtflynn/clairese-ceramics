d2c7ff0a612932e45519fdcc20faea93fc99aec7
Ashlynn Landry <ashclandry@gmail.com>
2022-12-18 00:40:24 -0500

    small fixes

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -43,8 +43,3 @@ export function idbPromise(storeName, method, object) {
         };
     });
 };
-
-export function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
