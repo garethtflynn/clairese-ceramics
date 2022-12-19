4b0b23d73badc8135133e15fc32d1ea288382334
Ashlynn Landry <ashclandry@gmail.com>
2022-12-18 19:28:09 -0500

    small change

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -48,25 +48,17 @@ function ProductItem(props) {
               {/*header*/}
               <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                 <h3 className="text-3xl font-semibold">
-                  Modal Title
+                  Mug Name
                 </h3>
                 <button
                   className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                   onClick={() => setShowModal(false)}
                 >
-                  <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                    ×
-                  </span>
                 </button>
               </div>
               {/*body*/}
               <div className="relative p-6 flex-auto">
                 <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                  I always felt like I could do anything. That’s the main
-                  thing people are controlled by! Thoughts- their perception
-                  of themselves! They're slowed down by their perception of
-                  themselves. If you're taught you can’t do anything, you
-                  won’t do anything. I was taught I could do everything.
                 </p>
               </div>
               {/*footer*/}
